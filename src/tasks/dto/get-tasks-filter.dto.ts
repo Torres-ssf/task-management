9f350c2b56301b3ea7fd634ec977d15dffc5d213
Torres-ssf <sergio.uft@gmail.com>
2020-12-05 20:12:23 -0300

    feat(tasks-filter.dto): use class-transform to add validation to fields at get-tasks-filter.dto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDTO {
+  @IsOptional()
+  @IsIn(Object.keys(TaskStatus))
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
