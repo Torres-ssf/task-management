c91e08794ae52b1b73c932f241f556e9feb7710f
Torres-ssf <sergio.uft@gmail.com>
2020-12-06 19:39:15 -0300

    feat(migrations): add migration to create tasks table

---
+++ src/migrations/1607287886428-CreateTasks.ts
@@ -0,0 +1,50 @@
+import { TaskStatus } from 'src/tasks/enums/task-status.enum';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTasks1607287886428 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'text',
+          },
+          {
+            name: 'status',
+            type: 'enum',
+            enum: [...Object.keys(TaskStatus)],
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks');
+  }
+}
