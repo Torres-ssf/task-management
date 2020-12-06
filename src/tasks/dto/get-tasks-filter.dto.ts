85f631fcb9ab4201aa59e32bf5f8a9a86bd48241
Torres-ssf <sergio.uft@gmail.com>
2020-12-06 19:46:49 -0300

    refactor(get-tasks-filter.dto): conform with TaskStatus enum file realocation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,5 @@
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../enums/task-status.enum';
 
 export class GetTasksFilterDTO {
   @IsOptional()
