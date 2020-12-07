a9f4bc1a2add27d798ee87c09987bb0a825032db
Torres-ssf <sergio.uft@gmail.com>
2020-12-07 20:20:13 -0300

    feat(auth-credentials.dto): create auth-credentials.dto

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Your password should have at least one symbol, one number, one lower case and one uppercase.',
+  })
+  password: string;
+}
