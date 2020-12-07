ffe866230f2bd04a4b915c0df2246127d897e6bb
Torres-ssf <sergio.uft@gmail.com>
2020-12-07 20:28:18 -0300

    feat(user.repository): implement method createNewUser

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 
+interface CreateUserParams {
+  name: string;
+  email: string;
+  password: string;
+  salt: string;
+}
+
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createNewUser({
+    name,
+    email,
+    password,
+    salt,
+  }: CreateUserParams): Promise<User> {
+    const user = this.create({
+      name,
+      email,
+      password,
+      salt,
+    });
+
+    return this.save(user);
+  }
+}
