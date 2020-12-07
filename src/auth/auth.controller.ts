be380ea185530848abbef25f959bfcdecc13a55d
Torres-ssf <sergio.uft@gmail.com>
2020-12-07 20:29:06 -0300

    feat(auth.controller): implement method signUp at auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { classToClass } from 'class-transformer';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  async signUp(
+    @Body(ValidationPipe) authCredentials: AuthCredentialsDto,
+  ): Promise<User> {
+    const user = await this.authService.signUp(authCredentials);
+
+    return classToClass(user);
+  }
+}
