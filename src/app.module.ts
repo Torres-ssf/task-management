dfb0fb3326d46d16b7fbe8bce2d7171f1833f17f
Torres-ssf <sergio.uft@gmail.com>
2020-12-06 23:31:28 -0300

    refactor(app.module): import auth.module at app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TasksModule],
+  imports: [TypeOrmModule.forRoot(), TasksModule, AuthModule],
 })
 export class AppModule {}
