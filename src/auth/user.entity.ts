45f5b86bd3ed524010b4c9b108abe787b0b19483
Torres-ssf <sergio.uft@gmail.com>
2020-12-07 20:19:23 -0300

    feat(user.entity): add name and salt columns to UserEntity

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,22 +1,33 @@
+import { Exclude } from 'class-transformer';
 import {
   BaseEntity,
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('users')
+@Unique(['email'])
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: string;
 
   @Column()
+  name: string;
+
+  @Column()
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
+
+  @Column()
+  @Exclude()
+  salt: string;
 
   @CreateDateColumn()
   created_at: Date;
