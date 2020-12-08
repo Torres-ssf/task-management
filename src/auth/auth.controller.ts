49d494d567e4326103dd27e71b0b5b0668e73a52
Torres-ssf <sergio.uft@gmail.com>
2020-12-07 23:20:13 -0300

    refactor(auth.controller): conform with rename of auth.crendentials.dto to sign-up-credentials.dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { classToClass } from 'class-transformer';
 import { AuthService } from './auth.service';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { SignUpCredentialsDto } from './dto/sign-up-credentials.dto';
 import { User } from './user.entity';
 
 @Controller('auth')
@@ -10,7 +10,7 @@ export class AuthController {
 
   @Post()
   async signUp(
-    @Body(ValidationPipe) authCredentials: AuthCredentialsDto,
+    @Body(ValidationPipe) authCredentials: SignUpCredentialsDto,
   ): Promise<User> {
     const user = await this.authService.signUp(authCredentials);
 
